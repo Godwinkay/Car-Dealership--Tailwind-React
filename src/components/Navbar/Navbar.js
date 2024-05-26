779a717ced37f2bfc835e66694c4f8055dce45d6
Godwinkay <117829382+Godwinkay@users.noreply.github.com>
2024-05-26 18:11:08 +0000

    nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -24,10 +24,10 @@ function Navbar() {
                 </div>
                 <div data-aos="fade=down" data-aos-once="true" data-aos-delay="300" className='hidden lg:flex justify-between items-center gap-4'>
                     <ul className='hidden lg:flex justify-between items-center gap-4'>
-                        <li className='text-xl p-2 mr-2 rounded-md font-bold text-blue-900 hover:bg-primary'><a href='/buy'></a>Buy</li>
-                        <li className='text-xl p-2 mr-2 rounded-md font-bold text-blue-900 hover:bg-primary'><a href='/rent'></a>Rent</li>
-                        <li className='text-xl p-2 mr-2 rounded-md font-bold text-blue-900 hover:bg-primary'><a href='/repair'>Repair</a></li>
-                        <li className='text-xl p-2 mr-2 rounded-md font-bold text-blue-900 hover:bg-primary'><a href='/spareparts'>SpareParts</a></li>
+                        <li className='text-xl p-2 mr-2 rounded-md font-bold text-blue-900 hover:bg-primary'><Link to='/buy'></Link>Buy</li>
+                        <li className='text-xl p-2 mr-2 rounded-md font-bold text-blue-900 hover:bg-primary'><Link to='/rent'></Link>Rent</li>
+                        <li className='text-xl p-2 mr-2 rounded-md font-bold text-blue-900 hover:bg-primary'><Link to='/repair'>Repair</Link></li>
+                        <li className='text-xl p-2 mr-2 rounded-md font-bold text-blue-900 hover:bg-primary'><Link to='/spareparts'>SpareParts</Link></li>
                     </ul>
                 </div>
                 <div className='absolute right-14 top-18 lg:hidden'>
@@ -39,10 +39,10 @@ function Navbar() {
                     open && (
                         <div className='lg:hidden'>
                         <ul className='bg-white space-y-3 p-4 rounded-md shadow-md absolute right-6 top-24 z-50'>
-                        <li className='text-md px-4 py-2 hover:bg-primary hover:text-white w-full rounded-md'><a href='/buy'></a>Buy</li>
-                        <li className='text-md px-4 py-2 hover:bg-primary hover:text-white w-full rounded-md'><a href='/rent'></a>Rent</li>
-                        <li className='text-md px-4 py-2 hover:bg-primary hover:text-white w-full rounded-md'><a href='/repair'>Repair</a></li>
-                        <li className='text-md px-4 py-2 hover:bg-primary hover:text-white w-full rounded-md'><a href='/spareparts'>SpareParts</a></li>
+                        <li className='text-md px-4 py-2 hover:bg-primary hover:text-white w-full rounded-md'><Link to='/buy'></Link>Buy</li>
+                        <li className='text-md px-4 py-2 hover:bg-primary hover:text-white w-full rounded-md'><Link to='/rent'></Link>Rent</li>
+                        <li className='text-md px-4 py-2 hover:bg-primary hover:text-white w-full rounded-md'><Link to='/repair'>Repair</Link></li>
+                        <li className='text-md px-4 py-2 hover:bg-primary hover:text-white w-full rounded-md'><Link to='/spareparts'>SpareParts</Link></li>
                     </ul>  
                         </div>
                     )
